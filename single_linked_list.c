de007cdbdf6c17c2ab3724a970c8d0ef0207fe15
Gangadhar <gangadhar4221@gmai.com>
2015-12-03 23:22:42 +0530

    added linked list functions addnodeafter and before element

--- single_linked_list.c
+++ single_linked_list.c
@@ -25,6 +25,66 @@ struct node *addatend(struct node *start,int data)
 	}
 	return start;
 }
+struct node *addatbefore(struct node *start,int data,int item)
+{
+	struct node *tmp;
+	struct node *p;
+	if(start->link == NULL && start->info == item)
+	{
+		tmp = (struct node*)malloc(sizeof(struct node));
+		tmp->info = data;
+		tmp->link = start;
+		start = tmp;
+		return start;
+	}
+	p = start;
+	while(p->link != NULL)
+	{
+		if(p->link->info == item)
+		{
+			tmp = (struct node*)malloc(sizeof(struct node));
+			tmp->info = data;
+			tmp->link = p->link;
+			p->link = tmp;				
+			printf("element found \n");
+			return start;
+		}	
+		p = p->link;
+	}
+	return start;
+}
+struct node *addatafter(struct node *start,int data,int item)
+{
+        struct node *tmp;
+	struct node *p = start;
+        if(start->link == NULL && start->info == item)
+        {
+                tmp = (struct node*)malloc(sizeof(struct node));
+                tmp->info=data;
+                tmp->link=start->link;
+                start->link=tmp;
+        }
+	while((p->info != item) && (p->link != NULL))
+	{
+		p=p->link;
+		
+	}
+	if(p->info == item)
+	{
+	        tmp = (struct node*)malloc(sizeof(struct node));
+	        tmp->info=data;
+        	tmp->link=p->link;
+	        p->link = tmp;
+	}
+	else
+	{
+		printf("Unable to add data element %d after %d ",data,item);
+		exit(1);
+	}
+        
+	return start;
+
+}
 struct node *addatbeg(struct node *start,int data)
 {
 	struct node *tmp;
@@ -69,5 +129,8 @@ int main()
 	start=addatbeg(start,20);
 	start=addatbeg(start,60);
 	start=addatend(start,70);
+	start=addatafter(start,30,20);
+	start=addatbefore(start,25,40);
+	//start=addatafter(start,2,5);
 	display(start);
 }
